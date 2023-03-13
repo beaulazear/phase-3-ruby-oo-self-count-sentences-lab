d2861ba7d94e6bbc1df3b1c3e6a42340d9785b6e
Beau <beaulazear@gmail.com>
2023-03-13 18:28:30 -0400

    commit tree e6ce32ed8986c9298c8b41fc79b259fda3d2ffdb

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    split_arr = self.split(/\.|\?|!/)
+    no_empty = split_arr.reject(&:empty?)
+    sentence_count = no_empty.length
+    sentence_count
   end
 end
